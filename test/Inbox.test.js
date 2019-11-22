16b7ee97b7655eadee5804e3c47d6f2c9fc65dbe
Rishivanth Thulasiraman <rishithulasiramanr@gmail.com>
2019-11-23 00:49:35 +0530

    fix interface issues

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,19 +2,23 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
     // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
 
-    web3.eth.getAccounts()
-     .then(fetchedAccounts => {
-         console.log(fetchedAccounts);
-     });
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .send({from: accounts[0], gas: '1000000'})
 });
 
 describe('Inbox', () => {
     it('deploys a contract',() => {
-
+        console.log(inbox);
     });
 });
+
 
