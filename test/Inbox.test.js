d33c0ad9ce7e7f506fb5831a9fced271cc46ff6e
Rishivanth Thulasiraman <rishithulasiramanr@gmail.com>
2019-11-23 00:28:23 +0530

    mocha test 1

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,3 +4,23 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
+class Car {
+    park() {
+        return 'stopped';
+    }
+    drive(){
+        return 'vroom';
+    }
+}
+
+describe('Car', () => {
+    it('can park',() => {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive',() => {
+        const car = new Car();
+        assert.equal(car.drive(), 'vroom');
+    })
+});
