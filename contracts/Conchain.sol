72a3360bfa04bc9355ae7d818fea3f92a4342bf9
Rishivanth Thulasiraman <rishithulasiramanr@gmail.com>
2019-11-26 03:34:24 +0530

    define reputation score

--- contracts/Conchain.sol
+++ contracts/Conchain.sol
@@ -3,13 +3,52 @@ pragma solidity ^0.4.25;
 contract ConchainMaster {
     address[] public deployedProjects;
 
+    struct User {
+        string userName ;
+        string role ;
+        string emailId ;
+        address userAddress ;
+        uint reputationScore ;
+    }
+
+    User[] public users;
+
+    mapping (string => address) nameResolver ;
+    mapping (string => address) emailResolver ;
+    mapping (address => User[]) addressResolver ;
+
     function createProject(string title) public returns(address deployedProject){
         address newProject = new ConchainProject(title, msg.sender);
         deployedProjects.push(newProject);
         return newProject;
     }
+
+    function registerUser(string name, string role, string emailId) public returns(bool status) {
+        User memory newUser = User ({
+            userName : name,
+            role : role,
+            emailId : emailId,
+            userAddress : msg.sender
+        });
+        users.push(newUser);
+        nameResolver[name] = msg.sender;
+        emailResolver[emailId] = msg.sender;
+        addressResolver[msg.sender] = newUser;
+    }
+
+    function updateReputation(address userAddress, bool direction) public {
+        if ( direction = true ) {
+            addressResolver[userAddress].reputationScore++;
+        }
+        else {
+            addressResolver[userAddress].reputationScore--;
+        }
+    }
 }
 
+
+
+/*
 contract ConchainProject {
     address public projectOwner;
     string public projectTitle;
@@ -20,3 +59,4 @@ contract ConchainProject {
     }
 
 }
+*/
